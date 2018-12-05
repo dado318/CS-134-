54409f2ed8b9d5e39fd4ee04afa69e94f7b18bc3
Davor Koret <dkoret318@gmail.com>
2018-12-05 12:54:34 -0800

    Add files via upload

---
+++ src/ofApp.h
@@ -0,0 +1,57 @@
+//
+//   CS 134 - Midterm Starter File - Fall 2018
+//
+//
+//   This file contains all the necessary startup code for the Midterm problem Part II
+//   Please make sure to you the required data files installed in your $OF/data directory.
+//
+//                                             (c) Kevin M. Smith  - 2018
+#pragma once
+
+#include "ofMain.h"
+#include  "ofxAssimpModelLoader.h"
+#include "ParticleSystem.h"
+#include "ParticleEmitter.h"
+
+
+class ofApp : public ofBaseApp{
+
+	public:
+		void setup();
+		void update();
+		void draw();
+
+		void keyPressed(int key);
+		void keyReleased(int key);
+		void mouseMoved(int x, int y );
+		void mouseDragged(int x, int y, int button);
+		void mousePressed(int x, int y, int button);
+		void mouseReleased(int x, int y, int button);
+		void mouseEntered(int x, int y);
+		void mouseExited(int x, int y);
+		void windowResized(int w, int h);
+		void dragEvent(ofDragInfo dragInfo);
+		void gotMessage(ofMessage msg);
+		void drawAxis(ofVec3f);
+		void initLightingAndMaterials();
+		void savePicture();
+		void toggleWireframeMode();
+		void togglePointsDisplay();
+
+		ofEasyCam cam;
+		ofxAssimpModelLoader lander;
+		ofLight light;
+		ofImage backgroundImage;
+		ofCamera *theCam = NULL;
+		ofCamera topCam;
+
+		bool bAltKeyDown;
+		bool bCtrlKeyDown;
+		bool bWireframe;
+		bool bDisplayPoints;
+	
+		bool bBackgroundLoaded = false;
+		bool bLanderLoaded = false;
+	
+
+};
