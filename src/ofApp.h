d13d7b73ff793bce07af3f43619668b57c7d9834
Davor Koret <dkoret318@gmail.com>
2018-12-05 22:13:56 -0800

    Update ofApp.h

--- src/ofApp.h
+++ src/ofApp.h
@@ -15,43 +15,48 @@
 
 
 class ofApp : public ofBaseApp{
-
-	public:
-		void setup();
-		void update();
-		void draw();
-
-		void keyPressed(int key);
-		void keyReleased(int key);
-		void mouseMoved(int x, int y );
-		void mouseDragged(int x, int y, int button);
-		void mousePressed(int x, int y, int button);
-		void mouseReleased(int x, int y, int button);
-		void mouseEntered(int x, int y);
-		void mouseExited(int x, int y);
-		void windowResized(int w, int h);
-		void dragEvent(ofDragInfo dragInfo);
-		void gotMessage(ofMessage msg);
-		void drawAxis(ofVec3f);
-		void initLightingAndMaterials();
-		void savePicture();
-		void toggleWireframeMode();
-		void togglePointsDisplay();
-
-		ofEasyCam cam;
-		ofxAssimpModelLoader lander;
-		ofLight light;
-		ofImage backgroundImage;
-		ofCamera *theCam = NULL;
-		ofCamera topCam;
-
-		bool bAltKeyDown;
-		bool bCtrlKeyDown;
-		bool bWireframe;
-		bool bDisplayPoints;
-	
-		bool bBackgroundLoaded = false;
-		bool bLanderLoaded = false;
-	
-
+    
+public:
+    void setup();
+    void update();
+    void draw();
+    
+    void keyPressed(int key);
+    void keyReleased(int key);
+    void mouseMoved(int x, int y );
+    void mouseDragged(int x, int y, int button);
+    void mousePressed(int x, int y, int button);
+    void mouseReleased(int x, int y, int button);
+    void mouseEntered(int x, int y);
+    void mouseExited(int x, int y);
+    void windowResized(int w, int h);
+    void dragEvent(ofDragInfo dragInfo);
+    void gotMessage(ofMessage msg);
+    void drawAxis(ofVec3f);
+    void initLightingAndMaterials();
+    void savePicture();
+    void toggleWireframeMode();
+    void togglePointsDisplay();
+    void playSound();
+    
+    ofEasyCam cam;
+    ofxAssimpModelLoader lander;
+    ofLight light;
+    ofImage backgroundImage;
+    ofCamera *theCam = NULL;
+    ofCamera topCam;
+    ofCamera sideCam;
+    
+    bool bAltKeyDown;
+    bool bCtrlKeyDown;
+    bool bWireframe;
+    bool bDisplayPoints;
+    
+    bool bBackgroundLoaded = false;
+    bool bLanderLoaded = false;
+    
+    ofSoundPlayer soundPlayer;
+    bool soundFileLoaded = false;
+    
+    ParticleEmitter emitter;
 };
